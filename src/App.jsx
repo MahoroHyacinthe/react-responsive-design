371fbcafa386b51feaa66211e9e0851d08a51437
Mahoro <lionmill3@gmail.com>
2025-07-28 16:25:29 +0200

    corrected somecodes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SectionComp from "./components/SectionComp"
 import SectionBComp from "./components/SectionBComp"
 import Mission from "./components/MIssion"
 import Lastpage from "./components/Lastpage"
-import Footercomp from "./components/FooterComp"
+import Footercomp from "./components/Footercomp"
 import Copyright from "./components/Copyright"
 function App() {
   return (
